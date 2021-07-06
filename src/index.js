6c01855f9c010ed16cf150fb17c667cef5cbd3b2
Eder <chlrobert93@gmail.com>
2021-07-06 12:07:35 -0500

    Estilos en línea JSX

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
+//Import the React and ReactDOM Libreries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+if (module.hot) {
+    module.hot.accept();
+  }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//Crear a react component
+const App = () =>{
+    return(
+        <div style={{backgroundColor:'yellow', height:'100vh'}}>
+            <label className="label" for="name">
+                Enter name:
+            </label>
+            <input id="name" type="text" />
+            <button style={{backgroundColor:'blue', color: 'white'}}>Submit</button>
+        </div> 
+
+    );
+}
+
+//Take the reactcomponent and show it on the screen
+ReactDOM.render(<App/>, document.querySelector('#root'));
